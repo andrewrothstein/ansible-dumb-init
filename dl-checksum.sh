f4385e6100c1d460f27a6927c96630c063945bd3
Andrew Rothstein <andrew.rothstein@gmail.com>
2020-05-11 13:56:42 -0400

    better bash hackery

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,27 +1,34 @@
 #!/usr/bin/env sh
-VER=${1:-1.2.2}
 DIR=~/Downloads
-APP=dumb-init
-MIRROR=https://github.com/Yelp/dumb-init/releases/download/v${VER}
-CHECKSUMS=${APP}_${VER}_checksums.txt
-CHECKSUMS_URL=$MIRROR/sha256sums
-if [ ! -e $DIR/$CHECKSUMS ];
-then
-    wget -q -O $DIR/$CHECKSUMS $CHECKSUMS_URL
-fi
+MIRROR=https://github.com/Yelp/dumb-init/releases/download
 
 dl()
 {
-    local arch=$1
-    local file=${APP}_${VER}_${arch}
-    local url=$MIRROR/$file
+    local ver=$1
+    local lchecksums=$2
+    local arch=$3
+    local file="dumb-init_${ver}_${arch}"
+    local url=$MIRROR/v${ver}/$file
     printf "    # %s\n" $url
-    printf "    %s: sha256:%s\n" $arch `egrep -e "$file\$" $DIR/$CHECKSUMS | awk '{print $1}'`
+    printf "    %s: sha256:%s\n" $arch $(egrep -e "$file\$" $lchecksums | awk '{print $1}')
 }
 
-printf "  # %s\n" $CHECKSUMS_URL
-printf "  '%s':\n" $VER
+dl_ver ()
+{
+    local ver=$1
+    local checksums="dumb-init_${ver}_checksums.txt"
+    local lchecksums=$DIR/$checksums
+    local checksums_url="$MIRROR/v${ver}/sha256sums"
+    if [ ! -e $lchecksums ];
+    then
+        wget -q -O $lchecksums $checksums_url
+    fi
+    printf "  # %s\n" $checksums_url
+    printf "  '%s':\n" $ver
 
-dl amd64
-dl ppc64el
-dl s390x
+    dl $ver $lchecksums amd64
+    dl $ver $lchecksums ppc64el
+    dl $ver $lchecksums s390x
+}
+
+dl_ver ${1:-1.2.2}
